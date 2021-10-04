9708f9b81b1de8e69a352125cf7b711ef0bd1dd7
plilhare <prateeklilhare@gmail.com>
2021-10-04 17:17:41 +0530

    included static root

--- jobSearch/settings.py
+++ jobSearch/settings.py
@@ -180,7 +180,7 @@ DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5!asj6yjpkag")
